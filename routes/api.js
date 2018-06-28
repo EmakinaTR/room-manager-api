e7b18a8c07ca8d937354690e3772901f7886cc1e
Talha Can Havadar <talhahavadar@hotmail.com>
2018-06-28 10:32:17 +0200

    remove unnecessary views and routes

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.json({
+      success: true,
+      message: 'Hi guys bro!! Welcome to our shiny api.'
+  });
 });
 
 module.exports = router;
