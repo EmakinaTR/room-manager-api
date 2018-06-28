c4741f64b9130bea93dd35f742c7f9149ab7f259
Yiğit Yüksel <yyu@emakina.com>
2018-06-28 17:09:23 +0300

    api-integration added.

--- app.js
+++ app.js
@@ -4,13 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var apiRouter = require('./routes/api');
-
+var roomManagementApiRouter = require('./google-calendar-api/googleCalendarApi');
 var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -18,7 +13,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/api', apiRouter);
+app.use('/googleCalendarApi', roomManagementApiRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -27,14 +22,13 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
-  var error = req.app.get('env') === 'development' ? err : {};
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.json({
-    message: err.message,
-    error: error
-  })
+  res.render('error');
 });
 
 module.exports = app;
