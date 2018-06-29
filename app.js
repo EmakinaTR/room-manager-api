720b9215b4b11fba1c98e3839513c2eb0ac9be34
Talha Can Havadar <talhahavadar@hotmail.com>
2018-06-29 17:16:34 +0200

    update oauth callback mechanism

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 var app = express();
 const {oauth2_handler} = require('./google-calendar-api');
 var apiRouter = require('./routes/api');
-
+var oauthRouter = oauth2_handler(app, express);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,7 +19,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api', apiRouter);
-app.use('/oauth', oauth2_handler(app, express));
+app.use('/oauth', oauthRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- google-calendar-api/oauth2_handler.js
+++ google-calendar-api/oauth2_handler.js
@@ -1,10 +1,17 @@
 const fs = require('fs')
-const readline = require('readline')
 const {google} = require('googleapis')
 const CREDENTIALS_PATH = './google-calendar-api/credentials.json'
 const CLIENT_SECRET_PATH = './google-calendar-api/client_secret.json'
-const SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
-var oAuth2Client = undefined
+const SCOPES = ['https://www.googleapis.com/auth/calendar']
+const oAuth2Client = createOAuth2Client(readClientSecret())
+
+oAuth2Client.on('tokens', (tokens) => {
+  if (tokens.refresh_token) {
+    // store the refresh_token in my database!
+    console.log('refresh_token', tokens.refresh_token);
+  }
+  console.log('access_token', tokens.access_token);
+});
 
 function readFile(path) {
   try {
@@ -24,9 +31,9 @@ function readClientSecret() {
   return readFile(CLIENT_SECRET_PATH)
 }
 
-function createOAuth2Client (client_secret) {
-  const {secret, client_id, redirect_uris} = client_secret.web
-  return new google.auth.OAuth2(client_id, secret, redirect_uris[0])
+function createOAuth2Client (client_secret_file) {
+  const {client_secret, client_id, redirect_uris} = client_secret_file.web
+  return new google.auth.OAuth2(client_id, client_secret, redirect_uris[0])
 }
 
 function redirectToAuthentication(oAuth2Client, res) {
@@ -43,43 +50,48 @@ module.exports = (app, express) => {
   let oauth = express.Router()
 
   app.use((req, res, next) => {
-    //TODO: check thar request is not for callback url
-    let token = readCredentials()
-    if (oAuth2Client === undefined) {
-      oAuth2Client = createOAuth2Client(readClientSecret())      
-    }
-    
-    if (token === undefined) {
-      redirectToAuthentication(oAuth2Client, res)
+    if (req.path !== '/oauth/callback') {
+      let token = readCredentials()
+      if (token === undefined) {
+        redirectToAuthentication(oAuth2Client, res)
+      } else {
+        oAuth2Client.setCredentials(token);
+        next();
+      }
     } else {
-      oAuth2Client.setCredentials(token);
       next();
     }
   })
 
-  oauth.get('/oauth2callback', (req, res) => {
-    console.log('oauth2callback request:', req.params)
-    oAuth2Client.getToken(code, (err, token) => {
+  oauth.get('/callback', (req, res) => {
+    console.log('oauth2callback request:', req.query.code)
+    oAuth2Client.getToken(req.query.code, (err, token) => {
+      console.log('err', err, 'token', token)
       if (err) {
         res.status = 500
-        reS.json({
-          error: err
+        res.json({
+          error: err.data
         })
         return
       }
 
       oAuth2Client.setCredentials(token);
+      console.log('callback token:', token)
+      res.status = 200
+      res.json({
+        success: true
+      })
       // Store the token to disk for later program executions
-      try {
-        fs.writeFileSync(CREDENTIALS_PATH, JSON.stringify(token));
-        console.log('Token stored to', CREDENTIALS_PATH);
-      } catch (err) {
-        console.error(err);
-        res.status = 500
-        res.json({
-          error: err
-        })
-      }
+      // try {
+      //   fs.writeFileSync(CREDENTIALS_PATH, JSON.stringify(token));
+      //   console.log('Token stored to', CREDENTIALS_PATH);
+      // } catch (err) {
+      //   console.error(err);
+      //   res.status = 500
+      //   res.json({
+      //     error: err
+      //   })
+      // }
     });
   })
 
