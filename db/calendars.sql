3e8d9a7255ebf8cce665645d067abbef5c44fb1f
Talha Can Havadar <talhahavadar@hotmail.com>
2018-07-03 10:00:39 +0200

    update fk action to cascade

--- db/calendars.sql
+++ db/calendars.sql
@@ -1,17 +1,23 @@
+-- Table: public.calendars
+
+-- DROP TABLE public.calendars;
+
 CREATE TABLE public.calendars
 (
     id integer NOT NULL,
-    calendar_id text NOT NULL,
+    calendar_id text COLLATE pg_catalog."default" NOT NULL,
     kiosk_id macaddr,
-    PRIMARY KEY (id),
+    CONSTRAINT calendars_pkey PRIMARY KEY (id),
+    CONSTRAINT unique_calendar_id UNIQUE (calendar_id),
     CONSTRAINT fk_kiosk FOREIGN KEY (kiosk_id)
         REFERENCES public.kiosks (id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
 )
 WITH (
     OIDS = FALSE
-);
+)
+TABLESPACE pg_default;
 
 ALTER TABLE public.calendars
     OWNER to ravawvkonfirwo;
